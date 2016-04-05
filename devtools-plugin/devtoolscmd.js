a47dabf78cf9016df7f28a7ca6affedd08698557
Andrew54757 <andrews54757@gmail.com>
2016-04-05 14:49:41 -0400

    Update devtoolscmd.js

--- devtools-plugin/devtoolscmd.js
+++ devtools-plugin/devtoolscmd.js
@@ -1,1 +1,53 @@
+'use strict';
 
+module.exports = function (gameServer, args) {
+  switch (args[1]) {
+    case 'hashFiles':
+      gameServer.updater.hashFiles();
+      console.log("[Console] Running hashfiles")
+      break;
+    case 'devMode':
+      if ( gameServer.config.dev == 1) {
+              gameServer.config.dev = 0;
+              console.log("[Console] Turned off devmode");
+        
+      } else {
+      gameServer.config.dev = 1;
+      console.log("[Console] Turned on devmode");
+      }
+      break;
+     case 'compileFilesJson':
+       console.log("[Console] Compiling files.json...");
+       
+       var fs = require('fs');
+var walk = function(dir, done) {
+  var results = [];
+  fs.readdir(dir, function(err, list) {
+    if (err) return done(err);
+    var i = 0;
+    (function next() {
+      var file = list[i++];
+      if (!file) return done(null, results);
+      file = dir + '/' + file;
+      fs.stat(file, function(err, stat) {
+        if (stat && stat.isDirectory()) {
+          walk(file, function(err, res) {
+            results = results.concat(res);
+            next();
+          });
+        } else {
+          results.push(file);
+          next();
+        }
+      });
+    })();
+  });
+};
+walk(../../../src)
+
+       
+       break;
+    default:
+      console.log('Unknown command, do hashFiles, devMode, or compileFilesJson');
+  }
+};
