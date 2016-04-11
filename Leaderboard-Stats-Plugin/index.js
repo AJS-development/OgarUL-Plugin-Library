40b0f01a66606d0a224d8b712e1295958fd363e3
Andrew54757 <andrews54757@gmail.com>
2016-04-11 16:30:52 -0400

    Update index.js

--- Leaderboard-Stats-Plugin/index.js
+++ Leaderboard-Stats-Plugin/index.js
@@ -56,12 +56,12 @@ if (plugin.stage == 0) {
  var time = new Date().toISOString();
   time.replace(/T/, ' ')     // replace T with a space
   time.replace(/\..+/, '')
-  lb[0] = "~~~~~~Stats~~~~~~";
+  lb[0] = "~~~~~Stats~~~~~";
   lb[1] = "Players: " + humans;
   lb[2] = "Minions: " + minions;
   lb[3] = "Bots: " + bots + " Time: ";
   lb[4] = time;
-  lb[5] = "~~~~~~~~~~~~~~~~~";
+  lb[5] = "~~~~~~~~~~~~~~~";
   if (plugin.tick > 5) {
     plugin.tick = 0;
     plugin.stage = 1;
@@ -73,11 +73,11 @@ if (plugin.stage == 0) {
   
   
 } else if (plugin.stage == 1) {
-  lb[0] = "~~~~~~Stats~~~~~~";
+  lb[0] = "~~~~~Stats~~~~~";
   lb[1] = "Highscore: " + Math.floor(gameServer.topscore);
   lb[2] = "By: " + gameServer.topusername;
   lb[3] = "Old HS Holder: " + Math.floor(gameServer.oldtopscores.score);
-  lb[4] = "~~~~~~~~~~~~~~~~~";
+  lb[4] = "~~~~~~~~~~~~~~~";
   
   if (plugin.tick > 5) {
     plugin.tick = 0;
