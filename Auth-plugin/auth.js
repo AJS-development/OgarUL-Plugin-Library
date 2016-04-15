0c97eb6900bd3fafb70f073cbe4d718845b3c8f2
Andrew54757 <andrews54757@gmail.com>
2016-04-15 08:31:38 -0400

    Create auth.js

---
+++ Auth-plugin/auth.js
@@ -0,0 +1,23 @@
+
+this.init = function(index) {
+this.index = index;
+}
+
+
+this.beforespawn = function (player) {
+
+
+};
+this.beforeeject = function(player) {
+
+
+};
+this.beforesplit = function(player) {
+
+
+};
+module.exports = this;
+
+
+
+
