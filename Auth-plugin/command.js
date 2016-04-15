782eae79170cb76c779b5b5c5205b5df061fc631
Andrew54757 <andrews54757@gmail.com>
2016-04-15 10:10:17 -0400

    Update command.js

--- Auth-plugin/command.js
+++ Auth-plugin/command.js
@@ -1,7 +1,14 @@
 module.exports = function(gameServer, split) {
   var c = split[1];
 if (c == 'power') {
-  
+ if (gameServer.auon == 1) {
+   gameServer.auon = 0;
+   console.log("[Console] Turned off plugin");
+   
+ } else {
+   gameServer.auon = 1;
+   console.log("[Console] Turned on plugin");
+ }
   
 }
 
