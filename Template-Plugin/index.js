ecd12688d6fde9bd2d9c4df0a07b162b78a5652f
Andrew54757 <andrews54757@gmail.com>
2016-04-15 17:33:21 -0400

    Update index.js

--- Template-Plugin/index.js
+++ Template-Plugin/index.js
@@ -1,38 +1,38 @@
 'use strict';   // dont touch
 var plugin = []; // dont touch
-plugin.command = []; // dont touch
-plugin.commandName = []; // dont touch
-plugin.gamemodeId = []; // dont touch
-plugin.gamemode = []; // dont touch
-plugin.addToHelp = []; // dont touch
+this.command = []; // dont touch
+this.commandName = []; // dont touch
+this.gamemodeId = []; // dont touch
+this.gamemode = []; // dont touch
+this.addToHelp = []; // dont touch
 
 // [General]
-plugin.name = ""; // Name of plugin REQUIRED
-plugin.author = ""; // author REQUIRED
-plugin.description = ''; // Desciprtion
-plugin.compatVersion = ''; // compatable with
-plugin.version = ''; // version REQUIRED
+this.name = ""; // Name of plugin REQUIRED
+this.author = ""; // author REQUIRED
+this.description = ''; // Desciprtion
+this.compatVersion = ''; // compatable with (optional)
+this.version = ''; // version REQUIRED
 
 // [Extra Commands]
-plugin.commandName[0] = ""; // plugin add-on command names
-plugin.addToHelp[0] = ""; // help command add-on (adds this string to the help command)
-plugin.command[0] = ''; // extra command location
+this.commandName[0] = ""; // plugin add-on command names
+this.addToHelp[0] = ""; // help command add-on (adds this string to the help command)
+this.command[0] = ''; // extra command location
 
 // [Extra Gamemodes]
-plugin.gamemodeId[0] = ''; // gamemodeids of extra plugin gamemodes
-plugin.gamemode[0] = ''; // gamemode location
+this.gamemodeId[0] = ''; // gamemodeids of extra plugin gamemodes
+this.gamemode[0] = ''; // gamemode location
 
 // [Configs]
-plugin.config = {
+this.config = {
 // config1: 0,
   
   
 }
-plugin.configfile = 'config.ini'
+this.configfile = 'config.ini'
 
 
 // [Functions]
-plugin.init = function (gameServer, config) {
+this.init = function (gameServer, config) {
   this.config = config;
   
   // init, Used to do stuff such as overriding things
@@ -40,10 +40,10 @@ plugin.init = function (gameServer, config) {
 
 };
 
-plugin.onSecond = function (gameServer) {
+this.onSecond = function (gameServer) {
 
   // called every second
 };
 
 
-module.exports = plugin; // dont touch
+module.exports = this; // dont touch
