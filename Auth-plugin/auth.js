a4b18b6d14f32e9c69bff71bac3b3fdfb7ffd97a
Andrew54757 <andrews54757@gmail.com>
2016-04-15 10:47:35 -0400

    Update auth.js

--- Auth-plugin/auth.js
+++ Auth-plugin/auth.js
@@ -5,7 +5,7 @@ this.index = index;
 }
 
 
-this.beforespawn = function (player) {
+this.beforespawn = function (player,gameServer) {
 if (!player.auth) {
   player.frozen = true;
   if (!player.astage) player.astage = 0;
@@ -34,8 +34,8 @@ if (!player.auth) {
     
   } else if (player.astage == 6) {
     var ok = true;
-    for (var i in this.gameServer.account) {
-      if (this.gameServer.account[i].username == player.name) {
+    for (var i in gameServer.account) {
+      if (gameServer.account[i].username == player.name) {
         ok = false
         break;
       }
@@ -58,7 +58,7 @@ if (!player.auth) {
       pass: player.pass,
       
     };
-    this.gameServer.account.push(ac);
+    gameServer.account.push(ac);
     
     player.name = 'Success!, Press w to login'
     player.astage = 50;
@@ -69,15 +69,15 @@ if (!player.auth) {
 return true;
 
 };
-this.beforeeject = function(player) {
+this.beforeeject = function(player, gameServer) {
 if (player.astage > -1 && player.astage < 3) {
   player.cells.forEach((cell)=>this.removeNode(cell));
   player.astage ++;
   return false;
 } else if (player.astage == 3) {
   var ok = false;
-  for (var i in this.gameServer.account) {
-    var account = this.gameServer.account[i];
+  for (var i in gameServer.account) {
+    var account = gameServer.account[i];
    if (account && account.pass == player.pa && account.un == account.username) {
      ok = true;
      player.accountid = i;
@@ -98,35 +98,35 @@ if (player.astage > -1 && player.astage < 3) {
   player.astage = 100;
   return false
 } else if (player.astage == 5) {
-player.cells.forEach((cell)=>this.gameServer.removeNode(cell));
+player.cells.forEach((cell)=>gameServer.removeNode(cell));
 player.astage = 6;
 } else if (player.astage == 50) {
-  player.cells.forEach((cell)=>this.gameServer.removeNode(cell));
+  player.cells.forEach((cell)=>gameServer.removeNode(cell));
   player.astage = 0;
 } else if (player.astage == 7) {
-  player.cells.forEach((cell)=>this.gameServer.removeNode(cell));
+  player.cells.forEach((cell)=>gameServer.removeNode(cell));
   player.astage = 8;
   } else {
   return true;
 }
 return false;
 };
-this.beforesplit = function(player) {
+this.beforesplit = function(player,gameServer) {
 if (player.astage == 0 && this.index.config.allowregister == 1) {
-  player.cells.forEach((cell)=>this.gameServer.removeNode(cell));
+  player.cells.forEach((cell)=>gameServer.removeNode(cell));
   player.astage = 5;
 } else {
   return true;
 }
 return false;
 };
-this.beforeq = function(player) {
+this.beforeq = function(player, gameServer) {
   if (player.astage == 0 && this.index.config.requirelogin != 1) {
     player.frozen = false;
     player.name = player.aname;
     player.astage = 50
   } else if (player.astage > 0 && player.astage < 100) {
-    player.cells.forEach((cell)=>this.gameServer.removeNode(cell));
+    player.cells.forEach((cell)=>gameServer.removeNode(cell));
   player.astage = 0;
     
   }
