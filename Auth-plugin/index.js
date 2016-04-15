cce78f669c2259c842857f516215234dbdb282e5
Andrew54757 <andrews54757@gmail.com>
2016-04-15 10:07:09 -0400

    Update index.js

--- Auth-plugin/index.js
+++ Auth-plugin/index.js
@@ -1,40 +1,39 @@
 'use strict';   // dont touch
 const fs = require('fs');
 var auth = require('./auth.js');
-var plugin = []; // dont touch
-plugin.command = []; // dont touch
-plugin.commandName = []; // dont touch
-plugin.gamemodeId = []; // dont touch
-plugin.gamemode = []; // dont touch
-plugin.addToHelp = []; // dont touch
+this.command = []; // dont touch
+this.commandName = []; // dont touch
+this.gamemodeId = []; // dont touch
+this.gamemode = []; // dont touch
+this.addToHelp = []; // dont touch
 
 // [General]
-plugin.name = "Auth"; // Name of plugin REQUIRED
-plugin.author = "andrews54757"; // author REQUIRED
-plugin.description = 'An auth plugin'; // Desciprtion
-plugin.compatVersion = ''; // compatable with
-plugin.version = '1.0.0'; // version REQUIRED
+this.name = "Auth"; // Name of plugin REQUIRED
+this.author = "andrews54757"; // author REQUIRED
+this.description = 'An auth plugin'; // Desciprtion
+this.compatVersion = ''; // compatable with
+this.version = '1.0.0'; // version REQUIRED
 
 // [Extra Commands]
-plugin.commandName[0] = "auth"; // plugin add-on command names
-plugin.addToHelp[0] = "auth     : Auth plugin command"; // help command add-on (adds this string to the help command)
-plugin.command[0] = require('./auth.js'); // extra command location
+this.commandName[0] = "auth"; // plugin add-on command names
+this.addToHelp[0] = "auth     : Auth plugin command"; // help command add-on (adds this string to the help command)
+this.command[0] = require('./auth.js'); // extra command location
 
 // [Extra Gamemodes]
-plugin.gamemodeId[0] = ''; // gamemodeids of extra plugin gamemodes
-plugin.gamemode[0] = ''; // gamemode location
+this.gamemodeId[0] = ''; // gamemodeids of extra plugin gamemodes
+this.gamemode[0] = ''; // gamemode location
 
 // [Configs]
-plugin.config = {
+this.config = {
 requirelogin: 0,
 plugin: 1,
 allowregister: 1,
 }
-plugin.configfile = 'config.ini'
+this.configfile = 'config.ini'
 
 
 // [Functions]
-plugin.init = function (gameServer, config) {
+this.init = function (gameServer, config) {
   this.config = config;
   auth.init(plugin, gameServer);
   gameServer.beforespawn = function(player) {
@@ -54,10 +53,18 @@ plugin.init = function (gameServer, config) {
 
 };
 
-plugin.onSecond = function (gameServer) {
+this.onSecond = function (gameServer) {
+if (!this.up) this.up = 0;
+if (this.up < 100) {
+  this.up ++;
+  
+} else {
+  this.up = 0;
+  fs.writeFileSync('accounts.json', gameServer.account);
+}
 
   // called every second
 };
 
 
-module.exports = plugin; // dont touch
+module.exports = this; // dont touch
