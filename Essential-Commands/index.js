2ba8ab0eee489e92f71ebda27db86d29b642f749
Andrew54757 <andrews54757@gmail.com>
2016-05-04 17:15:47 -0400

    Update index.js

--- Essential-Commands/index.js
+++ Essential-Commands/index.js
@@ -8,15 +8,15 @@ this.addToHelp = []; // dont touch
 
 // [General]
 this.name = "Essential Commands"; // Name of plugin REQUIRED
-this.author = ""; // author REQUIRED
+this.author = "Andrews54757"; // author REQUIRED
 this.description = 'Adds essential commands to ogar unlimited'; // Desciprtion
 this.compatVersion = ''; // compatable with (optional)
 this.version = '1.0.0'; // version REQUIRED
 
 // [Extra Commands]
-this.commandName[0] = ""; // plugin add-on command names
-this.addToHelp[0] = ""; // help command add-on (adds this string to the help command)
-this.command[0] = ''; // extra command location
+this.commandName[0] = "tptoplayer"; // plugin add-on command names
+this.addToHelp[0] = "tptoplayer : tps to a player"; // help command add-on (adds this string to the help command)
+this.command[0] = require('./tptoplayer.js'); // extra command location
 
 // [Extra Gamemodes]
 this.gamemodeId[0] = ''; // gamemodeids of extra plugin gamemodes
