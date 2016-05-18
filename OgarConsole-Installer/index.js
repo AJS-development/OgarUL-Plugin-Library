b481a66c7451ab051d69df06c741c70bc14e9d49
LegiTPanda <LegitSoulja@users.noreply.github.com>
2016-05-17 19:14:32 -0600

    Update index.js

--- OgarConsole-Installer/index.js
+++ OgarConsole-Installer/index.js
@@ -1,7 +1,6 @@
+'use scrict';
+
 'use strict';   // dont touch
-var fs = require('fs');
-var request = require("request");
-var exec = require('child_process').exec;
 var plugin = []; // dont touch
 this.command = []; // dont touch
 this.commandName = []; // dont touch
@@ -10,17 +9,11 @@ this.gamemode = []; // dont touch
 this.addToHelp = []; // dont touch
 
 // [General]
-this.name = "OgarConsole-Installer"; // Name of plugin REQUIRED
-this.author = "Installer- Andrews54757, Console - LegitSoulja"; // author REQUIRED
-this.description = 'Adds the OgarConsole plugin by LegitSoulja'; // Desciprtion
+this.name = "OgarConsole"; // Name of plugin REQUIRED
+this.author = "LegitSoulja"; // author REQUIRED
+this.description = 'OgarConsole >> Control your Ogar Server'; // Desciprtion
 this.compatVersion = ''; // compatable with (optional)
-this.version = '1.0.0'; // version REQUIRED
-
-// [Extra Commands]
-this.commandName[0] = ""; // plugin add-on command names
-this.addToHelp[0] = "-password [password] : Log into OgarConsole"; // help command add-on (adds this string to the help command)
-this.addToHelp[1] = "-logout : Log out of OgarConsole";
-this.command[0] = ''; // extra command location
+this.version = '1.0.4'; // version REQUIRED
 
 // [Extra Gamemodes]
 this.gamemodeId[0] = ''; // gamemodeids of extra plugin gamemodes
@@ -30,54 +23,309 @@ this.gamemode[0] = ''; // gamemode location
 this.config = {
 // config1: 0,
   
-  
-}
-this.configfile = 'config.ini'
-this.download = function(url, dest) {
-request(url, function (error, response, body) {
-      if (!error && response.statusCode == 200 && body != "") {
-        fs.writeFile(dest, body, (err, res)=> {
-        });
-      }
-});
-}
+};
 
-// [Functions]
-this.init = function (gameServer, config) {
-  this.config = config;
-  if (!fs.existsSync('./cmd.php')) {
-    console.log("[Console] Downloading ogarConsole");
-    this.download('https://raw.githubusercontent.com/LegitSoulja/OgarConsole/plugin/oldindex.js','./oldindex.js');
-    this.download('https://raw.githubusercontent.com/LegitSoulja/OgarConsole/plugin/cmd.php','./cmd.php');
-    this.download('https://raw.githubusercontent.com/LegitSoulja/OgarConsole/plugin/index.js','./index.js');
-    this.download('https://raw.githubusercontent.com/LegitSoulja/OgarConsole/plugin/package.json','./package.json');
-    this.download('https://raw.githubusercontent.com/LegitSoulja/OgarConsole/plugin/README.md','./OgarConsoleReadme.md');
-    setTimeout(function() {
-     
-      console.log('[Update] Running npm install to install new node modules!');
-    var child = exec("npm install", function (error, stdout, stderr) {
-      if (error !== null) {
-        console.error('[Execution Error] Failed to run npm install  Reason: ', error);
-        console.error('[Execution Error] You should exit the server and run: npm install');
-      } else {
-        console.log("[Console] OgarConsole installed successfuly! Configs are located in index.js ")
-        process.exit(0);
-      }
-    });
-    }, 3000);
+// Configuration file
+this.configfile = 'config.ini';
+
+this.init = function(gameServer, config){
     
-  }
-  
-  
-  // init, Used to do stuff such as overriding things
+    this.gameServer = gameServer;
+    
+    var OgarConsole = function(gameServer) { 
+        this.gameServer = gameServer;
+            this.start();
+
+    };
+
+    OgarConsole.prototype.settings = function(){
+
+            this.serverPort = 1000;
+            this.requirePassword = true;
+            this.consolePassword = "OgarConsole123";
+            this.advanced = false;
+            this.allowExit = false;
+            this.allowRestart = false;
+            this.logFile = "./logs/console.log";
+            this.consoleFile = "/cmd.ejs";
+            this.json = require("./package.json");
+            this.version = this.json.version;
+            return this;
+
+    };
+            
+            var request = require("request"),
+            gameServer = gameServer,
+            express = require("express"),
+            app = express(),
+            fs = require("fs"),
+            server = require("http").createServer(app),
+            io = require("socket.io").listen(server),
+            exec = require("child_process").exec;
+
+    OgarConsole.prototype.start = function(){
+
+        var newUserPassword = function(){ this._password = ""; };
+        newUserPassword.prototype.setPassword = function(a){ this._password = a; };
+
+        if(this.settings().consolePassword.length < 3){
+
+            OgarConsole.prototype.log("Password must be more than 3 chars in length.");
+            OgarConsole.prototype.terminate();
+            return;
+
+        };
+
+        // Create new connection
+        try{
+
+            server.listen(this.settings().serverPort);
+            OgarConsole.prototype.log("Runnin on port >> " + this.settings().serverPort);
+
+        }catch(e){
+
+            OgarConsole.prototype.log("Could not start OgarConsole server. >> " + e);
+            OgarConsole.prototype.terminate();
+            return;
+
+        }
+
+        server.on('error', function(err){
+            OgarConsole.prototype.log("Could not listen on port " + this.settings().serverPort);
+            OgarConsole.prototype.log("Ogar and OgarConsole stopped..");
+
+            OgarConsole.prototype.terminate();
+            return;
+
+        });
+
+        // OgarConsole >> Listen for connections
+        app.get("/", function(req, res){
+
+            fs.readFile(__dirname + thisOgarConsole.settings().consoleFile, function(err, data){
+
+                if(!err){
+
+                    res.send("" + data);
+
+                }else{
+
+                    res.send("" + err.toString());
+                    return;
+
+                }
+
+            });
+
+        });
+
+        io.sockets.on("connection", function(socket){
+
+            var login = new newUserPassword();
+
+            socket.on("commandex", function(data){
+
+                var first = "";
+                var data = data;
+                var split = [];
+
+                if(!thisOgarConsole.advanced){
+
+                    try{
+
+                        if(data === ""){
+                            return;
+                        }
+
+                        split = data.split(" ");
+                        first = split[0].toLowerCase();
+
+                        if(first !== "-password" && thisOgarConsole.requirePassword){
+
+                            if(login._password.length > 4){
+
+                                if(login._password === thisOgarConsole.consolePassword){
+                                    gameServer.log.onCommand(data);
+                                    OgarConsole.prototype.sendCommand(data, login, socket);
+                                    return;
+
+                                }else{
+                                    socket.emit("input", "Invalid password. Please enter correct password!.");
+                                    return;
+
+                                }
+
+                            }else{
+                                socket.emit("input", "Please enter password. Usage: '-password password'.");
+                                return;
 
 
+                            }
+
+                        }else{
+                            OgarConsole.prototype.sendCommand("-password " + split[1], login, socket);
+                            return;
+
+                        }
+
+                    }catch(e){
+
+                        // Ogar callback if error..
+                        socket.emit("input", "[Ogar] " + e.toString());
+                        return;
+
+                    }
+
+                }else{
+
+                    /*
+                     * 
+                     * ADVANCED MODE ONLY!. ONLY ACTIVATE IF YOU WANT TO SEND PURE CMD COMMANDS.
+                     * 
+                     * ( *THIS DISABLES OGAR COMMANDS USE* )
+                     * 
+                     * 
+                     * * BEWARE THE DAMAGES THIS MAY CAUSE. 
+                     * 
+                     * * ALSO THERES NO PASSWORDS WHEN ACTIVATING THIS!!
+                     * 
+                     */
+
+                    exec(data, function(e,s,t){
+                        socket.emit('input', s);
+                        return;
+                    });
+
+                }
+
+            });
+
+        });
+
+    };
+
+    OgarConsole.prototype.sendCommand = function(array, login, socket){
+
+        var d = array;
+        var array = d.split(" ");
+        var first = array[0].toLowerCase();
+
+        switch(first){
+
+            case "-password":
+                if(this.settings().requirePassword){
+
+                    if(array.length === 2){
+
+                        if(array[1] === this.settings().consolePassword){
+
+                            if(login._password.length < 3){
+                                login.setPassword(array[1]);
+                                socket.emit("input", "Logged In.&#10;" + "Type 'help' for commands.");
+                                thisOgarConsole.log("User Logged In :: " + socket.handshake.address);
+                                return;
+
+                            }else{
+                                socket.emit("input", "You are already logged into the console!.");
+                                return;
+                            }
+
+                        }else{
+                            socket.emit("input", "Invalid password.");
+                            return;
+                        }
+
+                    }else{
+                        socket.emit("input", "Usage: '-password password'");
+                        return;  
+                    } 
+
+                }else{
+                    socket.emit("input", "This console doesn't require a password.");
+                    return;
+                }
+                break;
+
+            case "-logout":
+                login._password = "";
+                socket.emit("input", "You have been logged out. Please login to re'gain access.");
+                OgarConsole.prototype.log("User Logged Out :: " + socket.handshake.address);
+                return;
+
+            case "clr":
+                fs.truncate(this.settings().logFile, "", function(){});
+                return;
+
+            case "clear":
+                fs.truncate(this.settings().logFile, "", function(){});
+                return;
+
+            case "exit":
+                if(!this.settings().allowExit)
+                    socket.emit("input", "You are not allowed to terminate this console");
+                return;
+
+            case "stop":
+                if(!this.settings().allowExit)
+                    socket.emit("input", "You are not allowed to terminate this console");
+                return;
+
+            case "restart":
+                if(!this.settings().allowRestart)
+                    socket.emit("input", "You are not allowed to terminate this console");
+                return;
+
+        }
+
+        var execute = gameServer.consoleService.commands[first];
+
+        if(typeof execute !== 'undefined'){
+
+                execute(gameServer, array);
+                setTimeout(function(){
+
+                    fs.readFile(thisOgarConsole.logFile, function(err, data){
+
+                       if(!err){
+
+
+                            socket.emit("input", "[LOG" + data.toString());
+                            return;
+
+                       }
+
+                    });
+
+                }, 20);
+
+        }else{
+
+            socket.emit("input", "Invalid Command!. Try 'help' for a list of commands.");
+            return;
+
+        }
+
+
+    };
+
+    OgarConsole.prototype.terminate = function(){
+
+        gameServer.socketServer.close();
+        process.exit(1);
+        return;
+
+    };
+
+    OgarConsole.prototype.log = function(log){
+
+        console.log("[OgarConsole] >> " + log);
+        return;
+
+    };
+    
+    var thisOgarConsole = new OgarConsole(gameServer);
+    
 };
 
-this.onSecond = function (gameServer) {
 
-  // called every second
-};
-
-
-module.exports = this; // dont touch
+module.exports = this;
