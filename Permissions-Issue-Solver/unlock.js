b5e77f1a6336e3c2e627c50a29e79b8bc27c238b
Andrew54757 <andrews54757@gmail.com>
2016-05-25 20:53:12 -0400

    Update unlock.js

--- Permissions-Issue-Solver/unlock.js
+++ Permissions-Issue-Solver/unlock.js
@@ -1,1 +1,11 @@
-
+var exec = require('child_process').exec;
+module.exports = function(gameServer,split) {
+  var child = exec("sudo chmod -R 777 ../", function (error, stdout, stderr) {
+      if (error !== null) {
+        console.error('[Execution Error] Failed to run chmod  Reason: ', error);
+      } else {
+        console.log("[Console] Files unlocked succesfully!";
+      }
+    });
+  
+}
