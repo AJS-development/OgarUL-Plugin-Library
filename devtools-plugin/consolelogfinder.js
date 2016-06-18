23f24bb2c4680f09c5a8ce95ae086ba133f0ad13
Andrew54757 <andrews54757@gmail.com>
2016-06-18 19:56:54 -0400

    Update consolelogfinder.js

--- devtools-plugin/consolelogfinder.js
+++ devtools-plugin/consolelogfinder.js
@@ -26,13 +26,21 @@ if (files[i].indexOf("/node_modules/") != -1 || files[i].indexOf("/plugins/") !=
   var file = fs.readFileSync(files[i],"utf8");
 var startindex = 0;
 for (;1==1;) {
- var index = file.indexOf("console.log(\"", startindex) + 12;
-var ind = file.indexOf(");", index);
-if (index == 11 || ind == -1) break;
+ var index = file.indexOf("console.log(\"", startindex) + 13;
+var ind = file.indexOf("\");", index);
+if (index == 12 || ind == -1) break;
 startindex = ind;
 var tx = file.substr(index, ind - index);
 var split = tx.split("\n");
-if (!split[1]) result.push(tx);
+if (split[1]) continue;
+
+for (;1==1;) {
+ var n = tx.indexOf("\"") + 1;
+ var c = tx.indexOf("\"",n);
+ if (n == 0 || c == -1) break;
+  tx = tx.slice(n,c);
+}
+result.push(tx);
 }
 }
 var final = "";
