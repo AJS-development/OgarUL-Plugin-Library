7b5325746ea55ae18d698c9b2e0e6210280f2036
Andrew54757 <andrews54757@gmail.com>
2016-06-18 20:48:24 -0400

    Update combine.js

--- devtools-plugin/combine.js
+++ devtools-plugin/combine.js
@@ -1,5 +1,13 @@
+var fs = require('fs');
 module.exports = function(gameServer,split) {
-
-
-
+var fileone = fs.readFileSync('./console.txt',"utf8");
+var filetwo = fs.readFileSync('./translate.txt',"utf8");
+var one = fileone.split("\n");
+var two = filetwo.split("\n");
+var result = "";
+for (var i in one) {
+ var on = one[i];
+  var tw = two[i];
+  var s = "this." +  + on +
+}
 };
