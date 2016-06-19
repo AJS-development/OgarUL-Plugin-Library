99b97a08a21e1dd2b56fba52aa97f991b5b5e59d
Andrew54757 <andrews54757@gmail.com>
2016-06-18 21:56:47 -0400

    Update consolelogfinder.js

--- devtools-plugin/consolelogfinder.js
+++ devtools-plugin/consolelogfinder.js
@@ -27,9 +27,11 @@ if (files[i].indexOf("/node_modules/") != -1 || files[i].indexOf("/plugins/") !=
 var startindex = 0;
 for (;1==1;) {
  var index = file.indexOf("console.log(\"", startindex) + 13;
+ var elindex = file.indexOf("this.log(\"", startindex) + 10;
 var ind = file.indexOf("\");", index);
-if (index == 12 || ind == -1) break;
+if ((index == 12 && elindex == 9) || ind == -1) break;
 startindex = ind;
+if (!index) index = elindex
 var tx = file.substr(index, ind - index);
 var split = tx.split("\n");
 if (split[1]) continue;
